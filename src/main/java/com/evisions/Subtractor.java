a4ec7df6f0eab305e939e10807cd829d51269aa7
Gabe Clark <unrivalled.dh@gmail.com>
2017-07-07 11:01:38 -0700

    Added an untested double subtract method

--- src/main/java/com/evisions/Subtractor.java
+++ src/main/java/com/evisions/Subtractor.java
@@ -4,5 +4,8 @@ package com.evisions;
  * Created by gabe.clark on 7/7/17.
  */
 public class Subtractor {
+
     static int subtract(int a, int b) { return a - b; }
+
+    static double subtract(double a, double b) { return a - b; }
 }
